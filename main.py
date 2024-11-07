8ee10e3ec42cd15250b13c0544dc6af2e984bb25
Duong Nguyen <144302817+NguyenDuong163@users.noreply.github.com>
2024-11-07 08:58:58 +0700

    update heuristic + an example of solved matrix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ class SudokuSolver:
     # Sudoku configuration
     SUDOKU_SIZE = 9
     # Algorithm configuration
-    HEURISTIC_GOAL = SUDOKU_SIZE * 3
+    HEURISTIC_GOAL = SUDOKU_SIZE * SUDOKU_SIZE * 9 
     POPULATION_NUM = 1024
     MUTATION_RATE = 0.05
 
@@ -46,8 +46,29 @@ class SudokuSolver:
 
     # Duong
     def HeuristicFunction(self, mat):
-        # return score
-        pass
+        score = 0
+
+        # Check rows
+        for row in mat:
+            score += len(set(row))
+
+        # Check columns
+        for col in range(self.SUDOKU_SIZE):
+            col_set = set()
+            for row in range(self.SUDOKU_SIZE):
+                col_set.add(mat[row][col])
+            score += len(col_set)
+
+        # Check 3x3 grids
+        for grid_row in range(0, self.SUDOKU_SIZE, 3):
+            for grid_col in range(0, self.SUDOKU_SIZE, 3):
+                grid_set = set()
+                for row in range(3):
+                    for col in range(3):
+                        grid_set.add(mat[grid_row + row][grid_col + col])
+                score += len(grid_set)
+
+        return score
 
     # Tai
     def SelectionFunction(self):
@@ -86,6 +107,17 @@ initial_mat = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
                [1, 2, 3, 4, 5, 6, 7, 8, 9],
                [1, 2, 3, 4, 5, 6, 7, 8, 9],
                [1, 2, 3, 4, 5, 6, 7, 8, 9]]
+
+# An example of a solved matrix
+sovle_mat =   [[4, 9, 6, 1, 5, 7, 8, 2, 3],
+               [2, 1, 8, 3, 9, 6, 7, 5, 4],
+               [7, 5, 3, 2, 8, 4, 1, 6, 9],
+               [6, 4, 9, 8, 3, 1, 2, 7, 5],
+               [5, 3, 1, 6, 7, 2, 9, 4, 8],
+               [8, 2, 7, 5, 4, 9, 6, 3, 1],
+               [9, 6, 2, 4, 1, 5, 3, 8, 7],
+               [1, 8, 5, 7, 6, 3, 4, 9, 2],
+               [3, 7, 4, 9, 2, 8, 5, 1, 6]]
 
 if __name__ == '__main__':
     print('PyCharm')
